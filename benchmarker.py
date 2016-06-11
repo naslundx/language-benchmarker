b7c4c217f93b89ef91d9b4a3a8e223f36326c478
Marcus Näslund <naslundx@gmail.com>
2016-06-11 11:20:50 +0200

    Bug fixes and cleanup

--- benchmarker.py
+++ benchmarker.py
@@ -9,38 +9,50 @@ from benchmarkers import benchmarkerc, benchmarkercpp, benchmarkerpython, benchm
 
 # TODO Check all compiler, language versions etc. and print+store in log
 
-# Get languages
-languages = ["c", "cpp", "python3", "ooc"] # TODO read from config file
-suffixes = ["c", "cpp", "py", "ooc"]
+languages = ["c", "cpp", "python3"] # TODO read from config file
+items = ["helloworld"] # TODO read from config file
+iterations = 100
+results = []
 
-for i in range(0, len(languages)):
-	language = languages[i]
-	item = "helloworld" # TODO get all items and process in order
-	suffix = suffixes[i] # TODO read from config file
-	filename = item + "." + suffix
-	binpath = language + "/bin/"
-	srcpath = language + "/"
+# Run all items in all languages
+for language in languages:
+	print("Running: " + language)
+	current_results = []
 
-	if language=="c":
-		benchmarker = benchmarkerc(binpath, srcpath, item, suffix)
-	elif language=="cpp":
-		benchmarker = benchmarkercpp(binpath, srcpath, item, suffix)
-	elif language=="python3":
-		benchmarker = benchmarkerpython(binpath, srcpath, item, suffix)
-	elif language=="ooc":
-		benchmarker = benchmarkerooc(binpath, srcpath, item, suffix)
+	for item in items:
+		print("\t" + item)
 
-	result = benchmarker.prepare()
+		if language=="c":
+			benchmarker = benchmarkerc(item)
+		elif language=="cpp":
+			benchmarker = benchmarkercpp(item)
+		elif language=="python3":
+			benchmarker = benchmarkerpython(item)
+		elif language=="ooc":
+			benchmarker = benchmarkerooc(item)
 
-	if result:
-		start = time.time()
-		result = benchmarker.execute()
-		end = time.time()
+		result = benchmarker.prepare()
+
 		if result:
-			print(end - start)
-		else:
-			print("Running failed.")
-	else:
-		print("Compilation failed")
+			times = []
+			for it in range(0, iterations):
+				start = time.time()
+				result = benchmarker.execute()
+				end = time.time()
+				if result:
+					times.append(end - start)
+				else:
+					print("Running failed.")
 
-# TODO store all results, generate a nice table output
+			average = sum(times) / len(times)
+			current_results.append(average)
+			print("\tTime: " + str(round(average, 4)) + " s")
+		else:
+			print("Compilation failed")
+			current_results.append(0.0)
+	
+	results.append(current_results)
+
+# Generate a nice table output to file
+# TODO
+
--- benchmarkers.py
+++ benchmarkers.py
@@ -1,61 +1,61 @@
 import subprocess
 import os
 
-# TODO only supply language folder name and have the rest inside class
+FNULL = open(os.devnull, 'w')
 
 class benchmarkerc:
 	compiler = "gcc"
+	srcpath = "c/"
+	binpath = "c/bin/"
+	suffix = "c"
 
-	def __init__(self, binpath, srcpath, item, suffix):
+	def __init__(self, item):
 		# Put in all the neccessary stuff
-		self.binpath = binpath
-		self.srcpath = srcpath
 		self.item = item
-		self.suffix = suffix
 		self.filename = self.item + "." + self.suffix
 
 	def prepare(self):
 		# Languages like C, C++ need to compile, script languages need nothing?
 		if not os.path.exists(self.binpath):
 			os.makedirs(self.binpath)
-		result = subprocess.call([self.compiler, self.srcpath + self.filename, "-o", self.binpath + self.item])
+		result = subprocess.call([self.compiler, self.srcpath + self.filename, "-o", self.binpath + self.item], stdout=FNULL)
 		return result == 0
 
 	def execute(self):
 		# Run whatever has been done
-		result = subprocess.call(["./" + self.binpath + self.item], stdout=None) # TODO pipe away output
+		result = subprocess.call(["./" + self.binpath + self.item], stdout=FNULL)
 		return result == 0
 
 class benchmarkercpp:
 	compiler = "g++"
+	srcpath = "cpp/"
+	binpath = "cpp/bin/"
+	suffix = "cpp"
 
-	def __init__(self, binpath, srcpath, item, suffix):
+	def __init__(self, item):
 		# Put in all the neccessary stuff
-		self.binpath = binpath
-		self.srcpath = srcpath
 		self.item = item
-		self.suffix = suffix
 		self.filename = self.item + "." + self.suffix
 
 	def prepare(self):
 		# Languages like C, C++ need to compile, script languages need nothing?
 		if not os.path.exists(self.binpath):
 			os.makedirs(self.binpath)
-		result = subprocess.call([self.compiler, self.srcpath + self.filename, "-o", self.binpath + self.item])
+		result = subprocess.call([self.compiler, self.srcpath + self.filename, "-o", self.binpath + self.item], stdout=FNULL)
 		return result == 0
 
 	def execute(self):
 		# Run whatever has been done
-		result = subprocess.call(["./" + self.binpath + self.item], stdout=None) # TODO pipe away output
+		result = subprocess.call(["./" + self.binpath + self.item], stdout=FNULL)
 		return result == 0
 
 class benchmarkerpython:
-	def __init__(self, binpath, srcpath, item, suffix):
+	srcpath = "python3/"
+	suffix = "py"
+
+	def __init__(self, item):
 		# Put in all the neccessary stuff
-		self.binpath = binpath
-		self.srcpath = srcpath
 		self.item = item
-		self.suffix = suffix
 		self.filename = self.item + "." + self.suffix
 
 	def prepare(self):
@@ -64,18 +64,18 @@ class benchmarkerpython:
 
 	def execute(self):
 		# Run whatever has been done
-		result = subprocess.call(["python3", self.srcpath + self.filename], stdout=None) # TODO pipe away output
+		result = subprocess.call(["python3", self.srcpath + self.filename], stdout=FNULL)
 		return result == 0
 
 class benchmarkerooc:
 	compiler = "rock"
+	srcpath = "ooc/"
+	binpath = "ooc/bin/"
+	suffix = "ooc"
 
-	def __init__(self, binpath, srcpath, item, suffix):
+	def __init__(self, item):
 		# Put in all the neccessary stuff
-		self.binpath = binpath
-		self.srcpath = srcpath
 		self.item = item
-		self.suffix = suffix
 		self.filename = self.item + "." + self.suffix
 
 	def prepare(self):
@@ -84,10 +84,10 @@ class benchmarkerooc:
 			os.makedirs(self.binpath)
 		my_env = os.environ.copy()
 		my_env["OOC_LIBS"] = "/home/mnaslund/versioned/ooc/"
-		result = 0 #subprocess.call([self.compiler, "-x", "-q", self.srcpath + self.filename, "+o " + self.binpath], env=my_env)
+		result = subprocess.call([self.compiler, "-x", "-q", self.srcpath + self.filename, "+o " + self.binpath], env=my_env, stdout=FNULL)
 		return result == 0
 
 	def execute(self):
 		# Run whatever has been done
-		result = 0 #subprocess.call(["./ooc/run-ooc.sh"])
+		result = subprocess.call(["./ooc/run-ooc.sh"], stdout=FNULL)
 		return result == 0
