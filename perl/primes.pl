49447ccdb7bde3115d57c27cc7dd750b1e538835
Marcus Näslund <naslundx@gmail.com>
2016-06-18 19:33:17 +0200

    Added perl/primes

---
+++ perl/primes.pl
@@ -0,0 +1,15 @@
+$n=10000;
+my @primes = ();
+for($i=3;$i<=$n;$i++) 
+{ 
+    $is_prime = 1;
+    for($j=2;$j<=sqrt($i);$j++){
+        if($i % $j == 0){
+            $is_prime = 0;
+            break;
+        }
+    }
+    if($is_prime == 1) {
+	push @primes, $i;
+    }
+}
